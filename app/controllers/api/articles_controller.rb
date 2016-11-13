94abcec9e8d1244eacf25bff7f69eb75cbd1b906
gcaaa31928 <gcaaa31928@gmail.com>
2016-11-14 01:47:42 +0800

    added article edit route

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -2,7 +2,39 @@ class Api::ArticlesController < ApplicationController
     def create
         retrieve
         permitted = params.permit(:title, :desc, :url, :date)
+        article = Article.new(title: permitted[:title],
+                              desc: permitted[:desc],
+                              url: permitted[:url],
+                              date: permitted[:date])
+        article.save!
+        render HttpStatusCode.ok
+    rescue => e
+        render HttpStatusCode.forbidden(
+            {
+                errorMsg: "#{$!}"
+            }
+        )
+    end
 
+    def edit
+        permitted = params.permit(:id, :title, :desc, :url, :date)
+        article = Article.find_by(id: permitted[:id].to_i)
+        article.update_attributes(title: permitted[:title],
+                                  desc: permitted[:desc],
+                                  url: permitted[:url],
+                                  date: permitted[:date])
+        render HttpStatusCode.ok
+    rescue => e
+        render HttpStatusCode.forbidden(
+            {
+                errorMsg: "#{$!}"
+            }
+        )
+    end
+
+    def all
+        articles = Article.all
+        render HttpStatusCode.ok(articles.as_json(except: [:created_at, :updated_at]))
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :api do
+        post 'articles/create' => 'articles#create'
+        post 'articles/edit' => 'articles#edit'
+        get 'articles' => 'articles#all'
+
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
