3bee7edb66f5e5e0ebefb5af9bedbdede732a8c6
gcaaa31928 <gcaaa31928@gmail.com>
2016-12-02 03:40:50 +0800

    added issue component

---
+++ public/app/issue/issue.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'issue',
+    templateUrl: './app/issue/issue.html',
+    styleUrls: ['./app/issue/issue.css']
+})
+
+export class IssueComopnent {
+
+}
