86048aebf84070d2fc4ec9304ebc7419836d77b6
gcaaa31928 <gcaaa31928@gmail.com>
2016-12-13 10:20:21 +0800

    added issue detail page

---
+++ public/app/issue-detail/issue-detail.component.ts
@@ -0,0 +1,16 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'issue-detail',
+    templateUrl: './app/issue-detail/issue-detail.html',
+    styleUrls: ['./app/issue-detail/issue-detail.css']
+})
+
+export class IssueDetailComponent {
+
+    constructor() {
+    }
+
+
+
+}
